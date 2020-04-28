409e2a78e5d71335e06cb51f859d9512705c777a
Megan McD <mcdevittbass@gmail.com>
2020-04-27 22:31:26 -0400

    Updates

---
+++ precache-manifest.1f7983e76c55ad06ce81896ddc093779.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b34a25c7118fa651bbb49fc8586aa42f",
+    "url": "/FivePointsReact/index.html"
+  },
+  {
+    "revision": "d27d321d64f9b7472984",
+    "url": "/FivePointsReact/static/css/2.e248f5df.chunk.css"
+  },
+  {
+    "revision": "c73859e2d885299b8781",
+    "url": "/FivePointsReact/static/css/main.24c9cde5.chunk.css"
+  },
+  {
+    "revision": "d27d321d64f9b7472984",
+    "url": "/FivePointsReact/static/js/2.9f203e63.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/FivePointsReact/static/js/2.9f203e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c73859e2d885299b8781",
+    "url": "/FivePointsReact/static/js/main.141218ac.chunk.js"
+  },
+  {
+    "revision": "26b7676a3e66dc89355e",
+    "url": "/FivePointsReact/static/js/runtime-main.703e1dcf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/FivePointsReact/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/FivePointsReact/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/FivePointsReact/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/FivePointsReact/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/FivePointsReact/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
