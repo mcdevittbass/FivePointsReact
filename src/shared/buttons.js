55c24f7a1f6f7630f797ea453494963dc6e271e0
Megan McD <mcdevittbass@gmail.com>
2020-04-30 08:45:29 -0400

    menu links

--- src/shared/buttons.js
+++ src/shared/buttons.js
@@ -6,20 +6,21 @@ export const BUTTONS = [
         src: baseUrl + '/img/menu_button.jpg',
         alt: 'menu button',
         id: 'menuButton',
-        href: '/img/five_points_menu.pdf',
+        href: baseUrl + '/img/five_points_menu.pdf',
         key: '0'
     },
     {
         src: baseUrl + '/img/farm_pantry.jpg',
         alt: 'farmers pantry menu button',
         id: 'farmButton',
-        href: '/img/Farmers Pantry - Feb 28 2020 - 8-23 PM.pdf',
+        href: baseUrl + '/img/Farmers Pantry - Feb 28 2020 - 8-23 PM.pdf',
         key: '1'
     },
     {
         src: baseUrl + '/img/FoundingButton.png',
         alt: 'founding story zine button',
         id: 'zineButton',
+        href: null,
         key: '2'
     }
 ]
