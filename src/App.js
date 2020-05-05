9e032e458819c45f66576937ff6bf2d13fb74eee
Megan McD <mcdevittbass@gmail.com>
2020-05-05 11:00:15 -0400

    Remove basename

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { HashRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <Router basename="/FivePointsReact">
+    <Router>
         <Main />
     </Router>
   );
